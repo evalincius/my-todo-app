5d93e3c6f07bc1eccd12ea8584bedf1d2bf41924
evalincius <edgaras.valincius@qa.com>
2022-06-23 12:39:26 +0100

    added delete endpoint

--- src/main/java/com/example/todo/controllers/TaskController.java
+++ src/main/java/com/example/todo/controllers/TaskController.java
@@ -2,7 +2,9 @@ package com.example.todo.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,11 @@ public class TaskController {
     @GetMapping("/all")
     public List<Task> getListOfTasks(){
         return taskRepository.findAll();
+    }  
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteTask(@PathVariable Long id){
+        taskRepository.deleteById(id);
+        return true;
     }
 }
