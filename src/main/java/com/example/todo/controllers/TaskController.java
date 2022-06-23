973c814d71efd4eb216fd6946823e874ae58defd
evalincius <edgaras.valincius@qa.com>
2022-06-23 16:05:00 +0100

    Added update endpoint

--- src/main/java/com/example/todo/controllers/TaskController.java
+++ src/main/java/com/example/todo/controllers/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,10 @@ public class TaskController {
     public Boolean deleteTask(@PathVariable Long id){
         taskRepository.deleteById(id);
         return true;
+    }
+
+    @PutMapping
+    public Task updateTask(@RequestBody Task task) {
+        return taskRepository.saveAndFlush(task);
     }
 }
