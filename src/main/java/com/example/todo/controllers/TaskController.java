d4200223ff51629c97f890f01c9eac36c1fe5f68
evalincius <edgaras.valincius@qa.com>
2022-07-07 12:17:18 +0100

    created fetch api

--- src/main/java/com/example/todo/controllers/TaskController.java
+++ src/main/java/com/example/todo/controllers/TaskController.java
@@ -2,7 +2,6 @@ package com.example.todo.controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,6 @@ import com.example.todo.repository.TaskRepository;
 
 @RestController
 @RequestMapping("/task")
-@CrossOrigin() 
 public class TaskController {
 
     private final TaskRepository taskRepository;
